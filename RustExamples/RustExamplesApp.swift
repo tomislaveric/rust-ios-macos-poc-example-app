a0e72ef3ad5f07aef85ca7d05c0439a46e4e278b
Tomislav Eric <hi@tomislav.mobi>
2023-12-20 12:16:04 +0100

    Initial Commit

---
+++ RustExamples/RustExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RustExamplesApp.swift
+//  RustExamples
+//
+//  Created by Tomislav Eric on 20.12.23.
+//
+
+import SwiftUI
+
+@main
+struct RustExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
